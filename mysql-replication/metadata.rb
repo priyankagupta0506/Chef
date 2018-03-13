6f288f5eb91104be9da658554ad9c6b9aad8bf80
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-03-13 17:03:40 +0530

    Add files via upload

---
+++ mysql-replication/metadata.rb
@@ -0,0 +1,17 @@
+name 'mysql-replication'
+maintainer 'Pavel Yudin'
+maintainer_email 'pyudin@parallels.com'
+license 'Apache 2.0'
+description 'Installs/Configures mysql-replication'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '2.0.0'
+issues_url 'https://github.com/parallels-cookbooks/mysql-replication/issues'
+source_url 'https://github.com/parallels-cookbooks/mysql-replication'
+
+supports 'amazon'
+supports 'redhat'
+supports 'centos'
+supports 'scientific'
+supports 'fedora'
+supports 'debian'
+supports 'ubuntu'
