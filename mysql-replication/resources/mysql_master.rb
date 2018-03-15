f829db9c267e93d179e25712027d40425b7867c3
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-03-15 15:16:18 +0530

    Update mysql_master.rb

--- mysql-replication/resources/mysql_master.rb
+++ mysql-replication/resources/mysql_master.rb
@@ -1,27 +1,9 @@
-#
-# Cookbook Name:: mysql-replication
-# Resource:: mysql_master
-#
-# Copyright 2015 Pavel Yudin
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 property :instance, kind_of: String, name_attribute: true
 property :id, kind_of: Integer
 property :log_bin, kind_of: String, default: 'mysql-bin'
-property :user, kind_of: String, default: 'root'
+property :user, kind_of: String, default: 'repl'
 property :host, kind_of: String, default: '%'
-property :password, kind_of: String, required: true
+property :password, kind_of: String, required: false
 property :binlog_do_db, kind_of: [Array, String]
 property :binlog_ignore_db, kind_of: [Array, String]
 property :binlog_format, kind_of: String, default: 'MIXED'
@@ -42,15 +24,15 @@ action :create do
               binlog_ignore_db: new_resource.binlog_ignore_db,
               options: new_resource.options
     action :create
-    notifies :restart, "mysql_service[ops]", :immediately
+    notifies :restart, "mysql_service[#{new_resource.name}]", :immediately
   end
 
   execute 'Grant permissions' do
-    command "echo \" GRANT SELECT,REPLICATION CLIENT,RELOAD,REPLICATION SLAVE ON *.* TO 'root'@'%'
-             IDENTIFIED BY PASSWORD 'mysql' \" | mysql -S #{mysql_socket}"
-    environment 'MYSQL_PWD' => 'mysql'
-    not_if "echo \"SHOW GRANTS FOR 'root'@'%';\" | mysql -S #{mysql_socket}",
-           environment: { 'MYSQL_PWD' => 'mysql' }
+    command "echo \" GRANT SELECT,REPLICATION CLIENT,RELOAD,REPLICATION SLAVE ON *.* TO '#{new_resource.user}'@'#{new_resource.host}'
+             IDENTIFIED BY PASSWORD '#{mysql_password(new_resource.password)}' \" | mysql -S #{mysql_socket}"
+    environment 'MYSQL_PWD' => mysql_instance.initial_root_password
+    not_if "echo \"SHOW GRANTS FOR '#{new_resource.user}'@'#{new_resource.host}';\" | mysql -S #{mysql_socket}",
+           environment: { 'MYSQL_PWD' => mysql_instance.initial_root_password }
   end
 end
 
