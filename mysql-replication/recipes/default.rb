4832b5d7b360e8481d7a585ad21ae54367f99dd4
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-03-19 12:13:14 +0530

    Update default.rb

--- mysql-replication/recipes/default.rb
+++ mysql-replication/recipes/default.rb
@@ -4,7 +4,20 @@
 #
 # Copyright 2015 Pavel Yudin
 #
+mysql_service 'ops' do
+  version '5.5'
+  bind_address '0.0.0.0'
+  data_dir '/data'
+  initial_root_password 'mysql'
+  mysqld_options 'innodb_buffer_pool_size' => '64M'
+  action [:create, :start]
+end
 
-mysql_master "master" do
-  action [:create]
+mysql_master 'default' do
+  binlog_do_db %w(test1)
+  password 'mysql'
+end
+
+execute 'Create database test1' do
+  command "mysql -u root -h 127.0.0.1 -pmysql | echo 'create database if not exists test1;'"
 end
