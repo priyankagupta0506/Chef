30dee15d99dce12bd25c8aaa2f5ab36abc2eabea
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-03-19 17:39:30 +0530

    Update mysql_slave.rb

--- mysql-replication/resources/mysql_slave.rb
+++ mysql-replication/resources/mysql_slave.rb
@@ -2,22 +2,8 @@
 # Cookbook Name:: mysql-replication
 # Resource:: mysql_slave
 #
-# Copyright 2015 Pavel Yudin
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-property :instance, kind_of: String, name_attribute: true
-property :id, kind_of: Integer
+property :instance, kind_of: String, name_attribute: true ,default: 'ops'
+property :id, kind_of: Integer, default: 2
 property :master_host, kind_of: String, required: true
 property :master_port, kind_of: Integer, default: 3306
 property :user, kind_of: String, default: 'repl'
