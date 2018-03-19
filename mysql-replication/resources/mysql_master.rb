b7bd64c74410894d4ad90197d0d86d68ee6391be
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-03-19 12:14:09 +0530

    Update mysql_master.rb

--- mysql-replication/resources/mysql_master.rb
+++ mysql-replication/resources/mysql_master.rb
@@ -3,7 +3,7 @@ property :id, kind_of: Integer
 property :log_bin, kind_of: String, default: 'mysql-bin'
 property :user, kind_of: String, default: 'repl'
 property :host, kind_of: String, default: '%'
-property :password, kind_of: String, required: false
+property :password, kind_of: String, required: true
 property :binlog_do_db, kind_of: [Array, String]
 property :binlog_ignore_db, kind_of: [Array, String]
 property :binlog_format, kind_of: String, default: 'MIXED'
@@ -15,7 +15,7 @@ default_action :create
 action :create do
   mysql_config 'master' do
     cookbook 'mysql-replication'
-    instance new_resource.name
+    #instance new_resource.name
     source 'master.conf.erb'
     variables id: new_resource.id || node['ipaddress'].split('.').join(''),
               log_bin: new_resource.log_bin,
@@ -24,29 +24,34 @@ action :create do
               binlog_ignore_db: new_resource.binlog_ignore_db,
               options: new_resource.options
     action :create
-    notifies :restart, "mysql_service[#{new_resource.name}]", :immediately
+    notifies :restart, "mysql_service[ops]", :immediately
   end
+if node["platform"] == "ubuntu"
+  execute "Grant permissions" do
+    command " mysql -u root -h 127.0.0.1 -pmysql | echo \" GRANT SELECT,REPLICATION CLIENT,RELOAD,REPLICATION SLAVE ON *.* TO 'repl'@'%'
+             IDENTIFIED BY PASSWORD 'mysql' \""
+#    command 'mysql -u root -h 127.0.0.1 -pmysql -e "show databases"'
+#    command 'mysql -u root -h 127.0.0.1 -pmysql -e "CREATE USER "repl"@'%';"'
+#    exec(mysql -u root -h 127.0.0.1 -pmysql -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'127.0.0.1'
+#             IDENTIFIED BY PASSWORD 'mysql'") | mysql -S /var/run/mysql-ops/mysqld.sock)
 
-  execute 'Grant permissions' do
-    command "echo \" GRANT SELECT,REPLICATION CLIENT,RELOAD,REPLICATION SLAVE ON *.* TO '#{new_resource.user}'@'#{new_resource.host}'
-             IDENTIFIED BY PASSWORD '#{mysql_password(new_resource.password)}' \" | mysql -S #{mysql_socket}"
-    environment 'MYSQL_PWD' => mysql_instance.initial_root_password
-    not_if "echo \"SHOW GRANTS FOR '#{new_resource.user}'@'#{new_resource.host}';\" | mysql -S #{mysql_socket}",
-           environment: { 'MYSQL_PWD' => mysql_instance.initial_root_password }
+#    environment 'MYSQL_PWD' => mysql_service[ops].initial_root_password
+#           environment: { 'MYSQL_PWD' => mysql_service[ops].initial_root_password }
   end
+end
 end
 
 action :delete do
   mysql_config 'master' do
-    instance new_resource.name
+    #instance new_resource.name
     action :delete
   end
 
   execute 'Remove permissions' do
-    command "echo \"DROP USER '#{new_resource.user}'@'#{new_resource.host}';\" | mysql -S #{mysql_socket}"
-    environment 'MYSQL_PWD' => mysql_instance.initial_root_password
+    command "echo \"DROP USER '#{new_resource.user}'@'#{new_resource.host}';\" | mysql -S /var/run/mysql-ops/mysqld.sock"
+#    environment 'MYSQL_PWD' => mysql_instance.initial_root_password
     action :run
-    only_if "echo \"SHOW GRANTS FOR '#{new_resource.user}'@'#{new_resource.host}';\" | mysql -S #{mysql_socket}",
-            environment: { 'MYSQL_PWD' => mysql_instance.initial_root_password }
+#    only_if "echo \"SHOW GRANTS FOR '#{new_resource.user}'@'#{new_resource.host}';\" | mysql -S /var/run/mysql-ops/mysqld.sock",
+#            environment: { 'MYSQL_PWD' => mysql_instance.initial_root_password }
   end
 end
