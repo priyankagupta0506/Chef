64dba4995a3fb12775f344784f0840ab7b797326
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-03-20 13:39:45 +0530

    Update default.rb

--- mysql-replication/recipes/default.rb
+++ mysql-replication/recipes/default.rb
@@ -9,6 +9,7 @@ mysql_service 'ops' do
   bind_address '0.0.0.0'
   data_dir '/data'
   initial_root_password 'mysql'
+  socket '/var/run/mysql-ops/mysqld.sock'
   mysqld_options 'innodb_buffer_pool_size' => '64M'
   action [:create, :start]
 end
