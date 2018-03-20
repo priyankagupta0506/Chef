8734d32c6572d57e1de84d2b1014ec3bcb610e7c
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-03-20 14:55:31 +0530

    Update mysql_master.rb

--- mysql-replication/resources/mysql_master.rb
+++ mysql-replication/resources/mysql_master.rb
@@ -3,11 +3,11 @@ property :id, kind_of: Integer,default: '1'
 property :log_bin, kind_of: String, default: '/var/log/mysql/mysql-bin.log'
 property :user, kind_of: String, default: 'repl'
 property :host, kind_of: String, default: '%'
-property :password, kind_of: String, required: true, default: 'mysql'
+property :password, kind_of: String, required: false, default: 'mysql'
 property :binlog_do_db, kind_of: [Array, String], default: 'test1'
 property :binlog_ignore_db, kind_of: [Array, String], default: 'mysql'
 property :binlog_format, kind_of: String, default: 'MIXED'
-property :options, kind_of: Hash
+#property :options, kind_of: Hash
 
 provides :mysql_master
 default_action :create
@@ -22,15 +22,15 @@ action :create do
               binlog_format: new_resource.binlog_format,
               binlog_do_db: new_resource.binlog_do_db,
               binlog_ignore_db: new_resource.binlog_ignore_db,
-              options: new_resource.options
+             # options: new_resource.options
     action :create
     notifies :restart, "mysql_service[ops]", :immediately
   end
 bash 'Grant permissions' do
   code <<-EOH
-    mysql -u root -h 127.0.0.1 -pmysql -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%' IDENTIFIED BY PASSWORD 'mysql';"
-    mysql -u root -h 127.0.0.1 -pmysql -e "FLUSH PRIVILEGES;"
-    mysql -u root -h 127.0.0.1 -pmysql -e "CREATE DATABASE test1;"
+    mysql -u root -h 127.0.0.1 -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%' IDENTIFIED BY PASSWORD 'mysql';"
+    mysql -u root -h 127.0.0.1 -e "FLUSH PRIVILEGES;"
+    mysql -u root -h 127.0.0.1 -e "CREATE DATABASE test1;"
   EOH
 end
 end
