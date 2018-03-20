429bffe588b6d587f1b91dacac62164295eda4ed
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-03-20 16:45:41 +0530

    Update mysql_master.rb

--- mysql-replication/resources/mysql_master.rb
+++ mysql-replication/resources/mysql_master.rb
@@ -34,12 +34,12 @@ action :create do
 #end
 #end
 
-if node["platform"] == "ubuntu"
   execute "Grant permissions" do
     command "mysql -u root -h 127.0.0.1 | echo \" GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%'
              IDENTIFIED BY PASSWORD 'mysql' \" | echo \" FLUSH PRIVILEGES \" | echo \" CREATE DATABASE test1 \" | echo \" show master status \""
+    action: run
   end
-end
+
 end
 #    command 'mysql -u root -h 127.0.0.1 -e "show databases"'
 #    command 'mysql -u root -h 127.0.0.1 -e "CREATE USER "repl"@'%';"'
