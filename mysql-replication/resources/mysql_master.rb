2ad99a9a7bc83ba2d45f782e74cc59e847eb72ea
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-03-21 12:48:55 +0530

    Update mysql_master.rb

--- mysql-replication/resources/mysql_master.rb
+++ mysql-replication/resources/mysql_master.rb
@@ -31,7 +31,7 @@ bash 'Grant permissions' do
     mysql -u root -h 127.0.0.1 -S /var/run/mysql-ops/mysqld.sock --password="" -e "FLUSH PRIVILEGES;"
     mysql -u root -h 127.0.0.1 -S /var/run/mysql-ops/mysqld.sock --password="mysql" -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%' IDENTIFIED BY 'mysql';"
     mysql -u root -h 127.0.0.1 -S /var/run/mysql-ops/mysqld.sock --password="mysql" -e "FLUSH PRIVILEGES;"
-    mysql -u root --password="mysql" -h 127.0.0.1  -e "CREATE DATABASE test1;"
+    mysql -u root --password="mysql" -h 127.0.0.1  -e "show master status;"
   EOH
 end
 end
