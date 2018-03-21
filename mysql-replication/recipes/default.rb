a810fc0bce03fac3b6a1cb1898de3f80ee5d615e
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-03-21 14:59:49 +0530

    Update default.rb

--- mysql-replication/recipes/default.rb
+++ mysql-replication/recipes/default.rb
@@ -2,8 +2,6 @@
 # Cookbook Name:: mysql-replication
 # Recipe:: default
 #
-# Copyright 2015 Pavel Yudin
-#
 mysql_service 'ops' do
   version '5.5'
   bind_address '0.0.0.0'
@@ -19,7 +17,3 @@ mysql_master 'ops' do
   id 1
   instance 'ops'
 end
-
-#execute 'Create database test1' do
-#  command "mysql -u root -h 127.0.0.1 -pmysql | echo 'create database if not exists test1;'"
-#end
