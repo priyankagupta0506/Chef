2c4c0a34159a1c35972cc2bcd55532db12f174d7
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-03-27 13:37:08 +0530

    Update helpers.rb

--- mysql/libraries/helpers.rb
+++ mysql/libraries/helpers.rb
@@ -43,7 +43,7 @@ module MysqlCookbook
     end
 
     def default_data_dir
-      return "/var/lib/#{mysql_name}" if node['os'] == 'linux'
+      return "/var/lib/mysql" if node['os'] == 'linux'
       return "/opt/local/lib/#{mysql_name}" if node['os'] == 'solaris2'
       return "/var/db/#{mysql_name}" if node['os'] == 'freebsd'
     end
