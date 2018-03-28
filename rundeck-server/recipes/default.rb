4f7608de4b4bc11ce0fab20638e3069c26347e09
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-03-28 12:43:38 +0530

    Update default.rb

--- rundeck-server/recipes/default.rb
+++ rundeck-server/recipes/default.rb
@@ -1,30 +1,4 @@
 #chef :: default recipe
-
-provides :rundeck_server
-
-action :create do
-    cookbook 'rundeck-server'
-    action :create
-    bash 'Rundeck install and start' do
-      code <<-EOH
-        sudo su
-        apt-get install openjdk-8-jdk
-        java -version
-        wget http://dl.bintray.com/rundeck/rundeck-deb/rundeck_2.10.8-1-GA_all.deb
-        dpkg -i rundeck_2.10.8-1-GA_all.deb
-        service rundeckd status 
-      EOH
-    end
-end
-action :start do
-    cookbook 'rundeck-server'
-    action :start
-    notifies :restart, "rundeckd", :immediately
-    bash 'Rundeck install and start' do
-      code <<-EOH
-        service rundeckd status 
-        service rundeckd start && service rundeckd status
-        curl -I http://localhost:4440
-      EOH
-    end
+rundeck_server 'rundeck' do
+  action [:create, :start]
 end
