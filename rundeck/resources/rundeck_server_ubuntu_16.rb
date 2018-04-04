85dadc7445a6c779c832f33b0da6e298453b0267
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-04-04 16:23:03 +0530

    Update rundeck_server_ubuntu_16.rb

--- rundeck/resources/rundeck_server_ubuntu_16.rb
+++ rundeck/resources/rundeck_server_ubuntu_16.rb
@@ -8,19 +8,25 @@ action :create do
       code <<-EOH
         sudo su
         mkdir -p /home/ubuntu/test_000
-        touch /home/ubuntu/test.sh
-        echo -ne '\n' | sudo add-apt-repository ppa:webupd8team/java -y
-        sudo dpkg --configure -a
+        echo -ne '\n' | sudo add-apt-repository ppa:webupd8team/java
+        mkdir -p /home/ubuntu/test_001
         sudo apt-get update -y
+        mkdir -p /home/ubuntu/test_002
         yes y | sudo apt-get install oracle-java8-installer -y
-        sudo apt-get install oracle-java8-set-default -y
+        mkdir -p /home/ubuntu/test_003
+        yes y | sudo apt-get install oracle-java8-set-default -y
+        mkdir -p /home/ubuntu/test_004
         echo -ne '\n' | sudo update-alternatives --config java
+        mkdir -p /home/ubuntu/test_005
         sudo echo "JAVA_HOME="/usr/lib/jvm/java-8-oracle"" >> /etc/environment
         source /etc/environment
+        mkdir -p /home/ubuntu/test_006
         wget http://dl.bintray.com/rundeck/rundeck-deb/rundeck_2.10.8-1-GA_all.deb
         dpkg -i rundeck_2.10.8-1-GA_all.deb
+        mkdir -p /home/ubuntu/test_007
         sudo service rundeckd status
         sudo service rundeckd start
+        mkdir -p /home/ubuntu/test_008
         sudo netstat -antlp | grep 4440
       EOH
     end
