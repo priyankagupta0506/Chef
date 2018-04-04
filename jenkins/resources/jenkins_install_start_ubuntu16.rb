16f09c664c180d2efda2fceb13cb0878d557f125
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-04-04 14:58:13 +0530

    Update jenkins_install_start_ubuntu16.rb

--- jenkins/resources/jenkins_install_start_ubuntu16.rb
+++ jenkins/resources/jenkins_install_start_ubuntu16.rb
@@ -1,1 +1,47 @@
 #
+#chef :: default recipe
+
+provides :jenkins_server_16
+
+action :create do
+    bash 'Jenkins install and start' do
+      code <<-EOH
+        sudo su
+        
+        yes y | wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+        echo "add in source list!!"
+        echo "deb https://pkg.jenkins.io/debian binary/" >> /etc/apt/sources.list
+        echo "update again !!"
+        yes y Y | apt-get update 
+        yes y Y | apt-get install jenkins
+        sudo service jenkins status | sudo service jenkins start
+      EOH
+    end
+end
+action :start do
+    bash 'Jenkins install and start' do
+      code <<-EOH
+        service jenkins status 
+        service jenkins start
+      EOH
+    end
+    #notifies :start, "jenkins_server[jenkins]", :immediately
+end
+action :stop do
+    bash 'Jenkins install and start' do
+      code <<-EOH
+        service jenkins status 
+        service jenkins stop && service jenkins status
+      EOH
+    end
+    notifies :stop, "jenkins_server[jenkins]", :immediately
+end
+action :restart do
+    bash 'Jenkins install and start' do
+      code <<-EOH
+        service jenkins status 
+        service jenkins restart && service jenkins status
+      EOH
+    end
+    notifies :restart, "jenkins_server[jenkins]", :immediately
+end
