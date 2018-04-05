e8362408d2451df6797766d59ab570206244c648
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-04-05 15:42:49 +0530

    Update mysql-slave.rb

--- mysql-replication-00/resources/mysql-slave.rb
+++ mysql-replication-00/resources/mysql-slave.rb
@@ -7,8 +7,8 @@ action :create do
     bash 'Mysql master start' do
       code <<-EOH
         sudo su
-        mysql -u root -h 127.0.0.1 -pmysql -e "CHANGE MASTER TO MASTER_HOST = '35.171.206.5', MASTER_USER = 'repl', MASTER_PASSWORD = 'mysql', MASTER_LOG_FILE = 'mysql-bin.000001', MASTER_LOG_POS = 868;"
-        mysql -u root -h 127.0.0.1 -pmysql -e "start slave;"
+        mysql -u root -pmysql -e "CHANGE MASTER TO MASTER_HOST = '35.171.206.5', MASTER_USER = 'repl', MASTER_PASSWORD = 'mysql', MASTER_LOG_FILE = 'mysql-bin.000001', MASTER_LOG_POS = 868;"
+        mysql -u root -pmysql -e "start slave;"
       EOH
     end
 end
