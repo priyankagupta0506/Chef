4dcb8663167b2ae0f486907ad27806fa51ff72f3
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-04-05 19:29:05 +0530

    Update mysql_master.rb

--- mysql-replication-00/resources/mysql_master.rb
+++ mysql-replication-00/resources/mysql_master.rb
@@ -6,10 +6,12 @@ action :create do
     bash 'Mysql master start' do
       code <<-EOH
         sudo su
+        sudo sed --in-place '/bind-address/d' /etc/mysql/my.cnf
+        sudo sed --in-place '/log_bin/d' /etc/mysql/my.cnf        
+        echo "[mysqld]" >> /etc/mysql/my.cnf
         echo "server-id           = 1" >> /etc/mysql/my.cnf
         echo "log_bin           = /var/log/mysql/mysql-bin.log" >> /etc/mysql/my.cnf
         echo "binlog_do_db           = test1" >> /etc/mysql/my.cnf
-        sudo sed --in-place '/127.0.0.1/d' /etc/mysql/my.cnf
         bind=$(wget -qO- http://instance-data/latest/meta-data/public-ipv4)
         sudo service mysql restart
         mysql -u root --password="mysql" -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%' IDENTIFIED BY 'mysql';"
