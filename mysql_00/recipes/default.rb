7da90b8fbd35d73e3f45460efc2346d1e3d09298
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-04-05 10:45:03 +0530

    Update default.rb

--- mysql_00/recipes/default.rb
+++ mysql_00/recipes/default.rb
@@ -1,1 +1,9 @@
 #
+mysql_service_direct "" do
+  port '3306'
+  version '5.5'
+  bind_address '0.0.0.0'
+  mysqld_options 'innodb_buffer_pool_size' => '64M'
+  initial_root_password 'mysql'
+  action [:create, :start]
+end
