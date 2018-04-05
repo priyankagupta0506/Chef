896ab9978931139d8719ce540a6c6745d145712b
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-04-05 18:32:16 +0530

    Update mysql_master.rb

--- mysql-replication-00/resources/mysql_master.rb
+++ mysql-replication-00/resources/mysql_master.rb
@@ -9,6 +9,9 @@ action :create do
         echo "server-id = 1" >> /etc/mysql/my.cnf
         echo "log_bin = /var/log/mysql/mysql-bin.log" >> /etc/mysql/my.cnf
         echo "binlog_do_db = test1" >> /etc/mysql/my.cnf
+        sudo sed --in-place '/127.0.0.1/d' /etc/mysql/my.cnf
+        bind=$(wget -qO- http://instance-data/latest/meta-data/public-ipv4)
+        echo "bind-address           = $bind" >> /etc/mysql/my.cnf
         sudo service mysql restart
         mysql -u root --password="mysql" -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%' IDENTIFIED BY 'mysql';"
         mysql -u root --password="mysql" -e "FLUSH PRIVILEGES;"
