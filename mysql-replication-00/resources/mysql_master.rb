9783043cf6dde64e32efbce8129fc8383d34b76a
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-04-05 15:34:58 +0530

    Update mysql_master.rb

--- mysql-replication-00/resources/mysql_master.rb
+++ mysql-replication-00/resources/mysql_master.rb
@@ -1,1 +1,24 @@
 #
+provides :mysql_master
+default_action :create
+
+action :create do
+    bash 'Mysql master start' do
+      code <<-EOH
+        sudo su
+        mysql -u root --pmysql -e "FLUSH PRIVILEGES;"
+        mysql -u root --pmysql -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%' IDENTIFIED BY 'mysql';"
+        mysql -u root --pmysql -e "FLUSH PRIVILEGES;"
+        mysql -u root --pmysql -e "show master status;"
+      EOH
+    end
+end
+action :start do
+    bash 'Mysql install and start' do
+      code <<-EOH
+        service mysql status 
+        service mysql start && service mysql status
+      EOH
+    end
+    #notifies :start, "mysql_server_direct[mysql]", :immediately
+end
