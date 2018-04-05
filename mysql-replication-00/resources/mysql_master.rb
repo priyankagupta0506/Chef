50f7d79e3059b4926deae5f15dea637a76f6a370
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-04-05 18:54:36 +0530

    Update mysql_master.rb

--- mysql-replication-00/resources/mysql_master.rb
+++ mysql-replication-00/resources/mysql_master.rb
@@ -6,16 +6,20 @@ action :create do
     bash 'Mysql master start' do
       code <<-EOH
         sudo su
-        echo "server-id = 1" >> /etc/mysql/my.cnf
-        echo "log_bin = /var/log/mysql/mysql-bin.log" >> /etc/mysql/my.cnf
-        echo "binlog_do_db = test1" >> /etc/mysql/my.cnf
+        echo "server-id           = 1" >> /etc/mysql/my.cnf
+        echo "log_bin           = /var/log/mysql/mysql-bin.log" >> /etc/mysql/my.cnf
+        echo "binlog_do_db           = test1" >> /etc/mysql/my.cnf
         sudo sed --in-place '/127.0.0.1/d' /etc/mysql/my.cnf
         bind=$(wget -qO- http://instance-data/latest/meta-data/public-ipv4)
         echo "bind-address           = $bind" >> /etc/mysql/my.cnf
         sudo service mysql restart
         mysql -u root --password="mysql" -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%' IDENTIFIED BY 'mysql';"
         mysql -u root --password="mysql" -e "FLUSH PRIVILEGES;"
-        mysql -u root --password="mysql" -e "show master status;"
+        mysql -u root --password="mysql" -e "create database test1;"
+        mysql -u root --password="mysql" -e "use test1;"
+        mysql -u root --password="mysql" -e "FLUSH TABLES WITH READ LOCK;"
+        mysql -u root --password="mysql" -e "SHOW MASTER STATUS;"
+        mysql -u root --password="mysql" -e "UNLOCK TABLES;"
       EOH
     end
 end
