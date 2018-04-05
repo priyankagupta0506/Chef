4ae820dda3dbaa92f1e81b4dc04c4400d7236dd6
Priyanka Gupta <priyanka.gupta@talentica.com>
2018-04-05 10:54:02 +0530

    Update mysql_install_run.rb

--- mysql_00/resources/mysql_install_run.rb
+++ mysql_00/resources/mysql_install_run.rb
@@ -1,1 +1,49 @@
 #
+#chef :: default recipe
+
+provides :rundeck_server
+
+action :create do
+    bash 'Rundeck install and start' do
+      code <<-EOH
+        sudo su
+        hostname -f
+        sudo apt-get update -y
+        sudo apt-get upgrade -y
+        sudo apt-get install mysql-server-5.5 -y
+        sudo mysql_secure_installation
+        mysql --version
+        sudo mysql_install_db 
+        sudo service mysql status
+        sudo service mysql start
+        sudo netstat -antlp | grep 3306
+      EOH
+    end
+end
+action :start do
+    bash 'Mysql install and start' do
+      code <<-EOH
+        service mysql status 
+        service mysql start && service mysql status
+      EOH
+    end
+    #notifies :start, "mysql_server_direct[mysql]", :immediately
+end
+action :stop do
+    bash 'Rundeck install and start' do
+      code <<-EOH
+        service mysql status 
+        service mysql stop && service mysql status
+      EOH
+    end
+    notifies :stop, "mysql_server_direct[mysql]", :immediately
+end
+action :restart do
+    bash 'Rundeck install and start' do
+      code <<-EOH
+        service mysql status 
+        service mysql restart && service mysql status
+      EOH
+    end
+    notifies :restart, "mysql_server_direct[mysql]", :immediately
+end
